cd06fd0164fca170e965af38fca61e9ad9b16b6d
Anicai Ovidiu <ovi@0v1.ro>
2020-07-07 20:28:21 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,25 @@
+input.onButtonPressed(Button.AB, function () {
+    input.calibrateCompass()
+})
+let Deg = 0
+basic.showLeds(`
+    . . # . .
+    . . # # .
+    . # . . .
+    . # # # #
+    . . # . .
+    `)
+basic.forever(function () {
+    Deg = input.compassHeading()
+    if (Deg < 45) {
+        basic.showString("N")
+    } else if (Deg < 135) {
+        basic.showString("E")
+    } else if (Deg < 225) {
+        basic.showString("S")
+    } else if (Deg < 315) {
+        basic.showString("W")
+    } else {
+        basic.showString("N")
+    }
+})
